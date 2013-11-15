145b2102dbb1df8fa826b02dfb131e3402833fc1
Mason Gup <masongup@gmail.com>
2013-11-15 15:43:41 -0600

    Add a basic database connector
    Add fields to connect to the database and display the database's current
    migration version, using newly-added commands to the runner.

--- src/FluentMigrator.WpfGui/MainWindow.xaml.cs
+++ src/FluentMigrator.WpfGui/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,17 +13,89 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
 
 namespace FluentMigrator.WpfGui
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            SetDefaults();
             InitializeComponent();
+        }
+
+        private void SetDefaults()
+        {
+            ServerName = "(local)\\SQLEXPRESS";
+            DbName = "SalusUAT";
+            CurrentVersion = "(No Connection)";
+        }
+
+        public string ServerName { get; set; }
+        public string DbName { get; set; }
+        public string CurrentVersion { get; set; }
+
+        private void ConnectButtonClicked(object sender, RoutedEventArgs e)
+        {
+            string newVersion = "(No Connection)";
+            try
+            {
+                long versionReturned;
+                if (long.TryParse(ExecuteTask("showlatest"), out versionReturned))
+                    newVersion = GetVersionFromMigrationCode(versionReturned).ToString();
+            }
+            catch (SqlException) //do nothing if the database connection fails
+            {}
+            
+            CurrentVersion = newVersion;
+            NotifyPropertyChanged("CurrentVersion");
+        }
+
+        private string ExecuteTask(string task)
+        {
+            var catcher = new OutputCatcher();
+            new TaskExecutor(GetRunnerContext(catcher, task)).Execute();
+            return catcher.Output;
+        }
+
+        private RunnerContext GetRunnerContext(IAnnouncer announcer, string task)
+        {
+            return new RunnerContext(announcer)//expects to get an IAnnouncer to say what the results of the commands are
+            {
+                Database = "SqlServer2008",
+                Connection = GetConnectionString(),
+                Target = "",//path to the assembly holding the migrations
+                Task = task,//"migrate:newer",
+                Version = 0//version to migrate to as long
+            };
+        }
+
+        private string GetConnectionString()
+        {
+            return "Data Source=" + ServerName + ";Initial Catalog=" + DbName + ";Integrated Security=SSPI;MultipleActiveResultSets=True";
+        }
+
+        private Version GetVersionFromMigrationCode(long inputCode)
+        {
+            var inputBytes = BitConverter.GetBytes(inputCode);
+            int rev = BitConverter.ToUInt16(new[] { inputBytes[0], inputBytes[1] }, 0);
+            int build = BitConverter.ToUInt16(new[] { inputBytes[2], inputBytes[3] }, 0);
+            int minor = BitConverter.ToUInt16(new[] { inputBytes[4], inputBytes[5] }, 0);
+            int major = BitConverter.ToUInt16(new[] { inputBytes[6], inputBytes[7] }, 0);
+            return new Version(major, minor, build, rev);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ src/FluentMigrator.WpfGui/OutputCatcher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Runner;
+
+namespace FluentMigrator.WpfGui
+{
+    public class OutputCatcher : IAnnouncer
+    {
+        private StringBuilder _totalOutput;
+
+        public OutputCatcher()
+        {
+            _totalOutput = new StringBuilder();
+        }
+
+        public string Output { get { return _totalOutput.ToString(); } }
+
+        public void Heading(string message)
+        {
+            _totalOutput.Append(message);
+        }
+
+        public void Say(string message)
+        {
+            if (message == "Task completed." || message.StartsWith("Using Database"))
+                return;
+            _totalOutput.Append(message);
+        }
+
+        public void Emphasize(string message)
+        {
+            _totalOutput.Append(message);
+        }
+
+        public void Sql(string sql)
+        {
+            _totalOutput.Append(sql);
+        }
+
+        public void ElapsedTime(TimeSpan timeSpan)
+        {
+            _totalOutput.Append(timeSpan);
+        }
+
+        public void Error(string message)
+        {
+            _totalOutput.Append(message);
+        }
+
+        public void Write(string message, bool escaped)
+        {
+            _totalOutput.Append(message);
+        }
+    }
+}
